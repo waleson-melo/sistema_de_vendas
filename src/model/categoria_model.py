cff0f418a0f08dfeb4a1be97ee17869136660af7
waleson-melo <walesonmelo23@gmail.com>
2021-01-14 14:25:10 -0400

    Função insert_categoria

--- src/model/categoria_model.py
+++ src/model/categoria_model.py
@@ -2,8 +2,10 @@ import src.connection.connection_db as conn
 
 
 class CategoriaModel:
-    def __init__(self, nome):
+    def __init__(self, nome=""):
         self.nome = nome
+
+        self.dados = (self.nome,)
 
         self.conn = conn.Connection()
 
@@ -14,3 +16,24 @@ class CategoriaModel:
     @nome.setter
     def nome(self, var):
         self._nome = str(var).strip().upper()
+
+    def not_vazio(self):
+        if self.nome != "":
+            return True
+        else:
+            return False
+
+    def insert_categoria(self):
+        if self.not_vazio():
+            self.conn.connect_db()
+
+            self.conn.cursor.execute("""
+                INSERT INTO categorias (nome_cate)
+                VALUES (?)
+            """, self.dados)
+            print('inserido com sucesso')
+
+            self.conn.close_db()
+        else:
+            print('esta vazio')
+            return False
