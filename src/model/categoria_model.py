952893861ba904c14964e561a8f2b499c0c3e2d1
waleson-melo <walesonmelo23@gmail.com>
2021-01-14 14:01:32 -0400

    Metrodos getters e seters

--- src/model/categoria_model.py
+++ src/model/categoria_model.py
@@ -2,15 +2,15 @@ import src.connection.connection_db as conn
 
 
 class CategoriaModel:
-    def __init__(self):
+    def __init__(self, nome):
+        self.nome = nome
+
         self.conn = conn.Connection()
 
-    def insert_categoria(self, nome):
-        self.conn.connect_db()
-        last_id = self.conn.cursor.execute("""
-            INSERT INTO compras (valor_comp, data_comp, status_comp, fk_cliente)
-            VALUES (25.50, '14/01/2021', 'pendente', '1')
-        """).lastrowid
-        self.conn.close_db()
+    @property
+    def nome(self):
+        return self._nome
 
-        print(last_id)
+    @nome.setter
+    def nome(self, var):
+        self._nome = str(var).strip().upper()
