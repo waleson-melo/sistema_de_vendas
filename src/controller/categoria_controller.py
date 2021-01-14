6a6d194eba105f0af9263350d06c32ca44d9f723
waleson-melo <walesonmelo23@gmail.com>
2021-01-14 18:28:53 -0400

    Add funções de INSERT, UPDATE, DELETE, SELECT e SELECT ALL

--- src/controller/categoria_controller.py
+++ src/controller/categoria_controller.py
@@ -4,3 +4,28 @@ import src.model.categoria_model as cateModel
 class CategoriaController:
     def __init__(self):
         pass
+
+    @staticmethod
+    def insert(nome):
+        catm = cateModel.CategoriaModel(nome=nome)
+        return catm.insert_categoria()
+
+    @staticmethod
+    def update(codigo, nome):
+        catm = cateModel.CategoriaModel(codigo=codigo, nome=nome)
+        return catm.update_categoria()
+
+    @staticmethod
+    def delete(codigo):
+        catm = cateModel.CategoriaModel(codigo=codigo)
+        return catm.delete_categoria()
+
+    @staticmethod
+    def select(nome):
+        catm = cateModel.CategoriaModel(nome=nome)
+        return catm.select_categoria()
+
+    @staticmethod
+    def select_all():
+        catm = cateModel.CategoriaModel()
+        return catm.select_all_categorias()
